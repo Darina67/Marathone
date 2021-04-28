0657b3e7df0f22c939f7c36a1941ff941fad3a32
Darina67 <75675517+Darina67@users.noreply.github.com>
2021-04-28 10:35:10 +0300

    Добавьте файлы проекта.

---
+++ AuthorizationPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Marathone
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthorizationPage.xaml
+    /// </summary>
+    public partial class AuthorizationPage : Page
+    {
+        public AuthorizationPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnThird_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnSecond_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnFirst_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new FirstPage());
+        }
+
+        private void BtnLogin_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Marathone
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Manager.MainFrame = MainFrame;
+            MainFrame.Navigate(new AuthorizationPage());
+        }
+
+        private void BtnFirst_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnLogin_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnSecond_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnThird_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MainFrame_ContentRendered(object sender, EventArgs e)
+        {
+            if (Manager.MainFrame.CanGoBack)
+                back.Visibility = Visibility.Visible;
+            else
+                back.Visibility = Visibility.Hidden;
+        }
+
+       //коммент
+            
+        
+        private void back_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.GoBack();
+        }
+    }
+}
